c8635b600cdbe001726352565ca8cc5b29fe7613
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-05-24 15:54:03 +0200

    Add project files.

---
+++ ModbusDotnetCoreDemo/Program.cs
@@ -0,0 +1,69 @@
+﻿using NModbus;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ModbusDotnetCoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Task.Run(()=>ModbusTest());
+
+            Console.ReadKey();
+
+
+        }
+
+
+        private static async Task SlaveTest()
+        {
+            string hostname = "localhost";
+            int port = 9000;
+
+            var TcpHost = IPAddress.Parse("127.0.0.1");
+
+            var slaveListener = new TcpListener(TcpHost, port);
+            
+            var factory = new ModbusFactory();
+
+            using (var client = new TcpClient())
+            using (var slave = factory.CreateSlaveNetwork(slaveListener))
+            {
+                slave.AddSlave(factory.CreateSlave(1));
+
+                await slave.ListenAsync();
+
+            }
+        }
+
+        private static async Task  ModbusTest()
+        {
+            string hostname = "localhost";
+            int port = 502;
+
+            var factory = new ModbusFactory();
+
+            using (var client = new TcpClient())
+            using (var master = factory.CreateMaster(client))
+            {
+                await client.ConnectAsync(hostname, port);
+
+                var inputs = await master.ReadInputRegistersAsync(1, 30001, 10);
+
+                var outputs = new bool[] { true, false, true, true };
+
+                await master.WriteMultipleCoilsAsync(1, 17, outputs);
+
+                var outputs2 = await master.ReadCoilsAsync(1, 17, 10);
+            }
+
+        }
+
+    }
+}
